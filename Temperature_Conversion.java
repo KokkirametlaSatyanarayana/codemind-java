912d9107a99437c578bc88e97e9b73dee8a8db50
KokkirametlaSatyanarayana <97425594+KokkirametlaSatyanarayana@users.noreply.github.com>
2023-02-01 11:46:33 +0530

    import java.util.*; class basic {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         float x;         float y;         x=sc.nextInt();         y=32+(x*9/5);         System.out.format("%.2f",y);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class basic
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        float x;
+        float y;
+        x=sc.nextInt();
+        y=32+(x*9/5);
+        System.out.format("%.2f",y);
+    }
+}
