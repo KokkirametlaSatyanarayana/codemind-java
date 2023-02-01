c1a6572fd75092e29965356154261d0734ac05e1
KokkirametlaSatyanarayana <97425594+KokkirametlaSatyanarayana@users.noreply.github.com>
2023-02-01 12:26:10 +0530

    import java.util.*; class basic {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String x;         x=sc.nextLine();         System.out.println(x);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class basic
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String x;
+        x=sc.nextLine();
+        System.out.println(x);
+    }
+}
