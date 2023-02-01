29166b46586bc15d20200e52f8d0984e635a3e93
KokkirametlaSatyanarayana <97425594+KokkirametlaSatyanarayana@users.noreply.github.com>
2023-02-01 12:00:23 +0530

    import java.util.*; class basic {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         double x,y;         double m;         x=sc.nextFloat();         y=sc.nextFloat();         m=(x*y);         System.out.format("%.2f",m);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class basic
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        double x,y;
+        double m;
+        x=sc.nextFloat();
+        y=sc.nextFloat();
+        m=(x*y);
+        System.out.format("%.2f",m);
+    }
+}
