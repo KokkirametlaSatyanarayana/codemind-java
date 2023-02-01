64ea5c397e2228cbff69743c59c97ecb7dbbe9c9
KokkirametlaSatyanarayana <97425594+KokkirametlaSatyanarayana@users.noreply.github.com>
2023-02-01 12:02:47 +0530

    import java.util.*; class basic {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x,y,n;         x=sc.nextInt();         y=sc.nextInt();         n=(x*y);         System.out.println(n);     } }

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class basic
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x,y,n;
+        x=sc.nextInt();
+        y=sc.nextInt();
+        n=(x*y);
+        System.out.println(n);
+    }
+}
