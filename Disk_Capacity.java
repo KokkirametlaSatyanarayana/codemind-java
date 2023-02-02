e230cf4a6ba1eee6888636902b3c4fba33fc2126
KokkirametlaSatyanarayana <97425594+KokkirametlaSatyanarayana@users.noreply.github.com>
2023-02-02 10:31:50 +0530

    import java.util.*; class basixffqerfrfreioffofn {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x,y,z,m;         x=sc.nextInt();         y=sc.nextInt();         z=sc.nextInt();         m=x*y*z*1024;         System.out.println(m);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class basixffqerfrfreioffofn
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x,y,z,m;
+        x=sc.nextInt();
+        y=sc.nextInt();
+        z=sc.nextInt();
+        m=x*y*z*1024;
+        System.out.println(m);
+    }
+}
